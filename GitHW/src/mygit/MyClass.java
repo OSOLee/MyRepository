dd4bf3af8f757f7f28efe99a9d950d3a63be1bbd
oso821 <oso821@naver.com>
2025-12-03 14:03:07 +0900

    merge branch 'dev'

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -6,12 +6,9 @@ public class MyClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
 		System.out.println("This is 2nd version.");
-<<<<<<< HEAD
 		System.out.println("My changed in master branch");
-=======
 		YourClass your = new YourClass();
 		your.sayHello("Everyone");
->>>>>>> refs/heads/dev
 	}
 
 }
